f756eed679a67c05c10ae2fad8ba17549532bd7b
Mohamed Ameen <68771689+mohamedameen21@users.noreply.github.com>
2025-05-11 09:31:13 +0530

    Time: 0 ms (100.00%) | Memory: 46.6 MB (7.56%) - LeetSync

---
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.java
@@ -0,0 +1,78 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if(root == null) return root;
+        if(root.left == null && root.right == null) return key == root.val ? null : root;
+        if(root.val == key) return mergeSubtrees(root.left, root.right);
+
+        TreeNode parent = null;
+        TreeNode cur = root;
+
+        while (cur != null) {
+            if (cur.val == key)
+                break;
+            else if (key < cur.val) {
+                parent = cur;
+                cur = cur.left;
+            } else {
+                parent = cur;
+                cur = cur.right;
+            }
+        }
+
+        if(cur == null) return root;
+
+        boolean isLeftChild = parent.left == cur;
+
+        if(cur.left != null) {
+            if(isLeftChild) {
+                parent.left = cur.left;
+                parent.left.right = mergeSubtrees(parent.left.right, cur.right);
+            } else {
+                parent.right = cur.left;
+                parent.right.right = mergeSubtrees(parent.right.right, cur.right);
+            }
+        } else if(cur.right != null) {
+            if(isLeftChild) {
+                parent.left = cur.right;
+                parent.left.left = mergeSubtrees(cur.left, parent.left.left);
+            } else {
+                parent.right = cur.right;
+                parent.right.left = mergeSubtrees(cur.left, parent.right.left);
+            }
+        } else {
+            if(isLeftChild) parent.left = null;
+            else parent.right = null;
+        }
+
+        return root;
+    }
+
+    private static TreeNode mergeSubtrees(TreeNode leftRoot, TreeNode rightRoot) {
+        if(leftRoot == null) return rightRoot;
+        if(rightRoot == null) return leftRoot;
+
+        TreeNode cur = rightRoot;
+        while(cur.left != null) {
+            cur = cur.left;
+        }
+
+        cur.left = leftRoot;
+
+        return rightRoot;
+    }
+}
