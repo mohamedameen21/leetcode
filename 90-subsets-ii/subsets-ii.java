a3fd6b82ccc25ab9ddcc38664be648e1a2c4a79d
Mohamed Ameen <68771689+mohamedameen21@users.noreply.github.com>
2025-06-24 09:15:38 +0530

    Time: 2 ms (30.62%) | Memory: 44.9 MB (5.48%) - LeetSync

---
+++ 90-subsets-ii/subsets-ii.java
@@ -0,0 +1,28 @@
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        return getUniqueSubsets(nums,new ArrayList<>(), 0);
+    }
+
+    private List<List<Integer>> getUniqueSubsets(int[] nums, List<Integer> temp, int index) {
+        List<List<Integer>> list = new ArrayList<>();
+
+        if (index >= nums.length) {
+            List<Integer> l = new ArrayList<>(temp); // deep copy
+            list.add(l);
+            return list;
+        }
+
+        temp.add(nums[index]);
+        list.addAll(getUniqueSubsets(nums, new ArrayList<>(temp), index + 1));
+
+        temp.remove(temp.size() - 1);
+
+        while (index + 1 < nums.length && nums[index] == nums[index + 1])
+            index++;
+
+        list.addAll(getUniqueSubsets(nums, new ArrayList<>(temp), index + 1));
+
+        return list;
+    }
+}
