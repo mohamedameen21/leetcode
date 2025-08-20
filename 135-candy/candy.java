e76c56df9884315d5c519f49d33d6fe3d4c706dc
Mohamed Ameen <68771689+mohamedameen21@users.noreply.github.com>
2025-08-20 10:34:09 +0530

    Time: 2 ms (99.40%) | Memory: 45.9 MB (45.57%) - LeetSync

--- 135-candy/candy.java
+++ 135-candy/candy.java
@@ -1,31 +1,34 @@
 class Solution {
     public int candy(int[] ratings) {
-        int[] result = new int[ratings.length];
-        result[0] = 1;
-        int noOfCandies = 0;
+        int i = 1;
+        int sum = 1;
 
-        for(int i = 1; i < ratings.length; i++) {
-            if(ratings[i] > ratings[i -1]) {
-                result[i] = result[i - 1] + 1;
-            } else {
-                result[i] = 1;
+        while(i < ratings.length) {
+            while(i < ratings.length && ratings[i] == ratings[i - 1]) {
+                sum++;
+                i++;
+            }
+
+            int peak = 1;
+            while(i < ratings.length && ratings[i] > ratings[i -1]) {
+                peak++;
+                sum += peak;
+                i++;
+            }
+
+            int down = 0; 
+            while(i < ratings.length && ratings[i] < ratings[i-1]) {
+                down++;
+                sum += down;
+                i++;
+            }
+            down++;
+            
+            if(down > peak) {
+                sum += (down - peak);
             }
         }
 
-        System.out.println(Arrays.toString(result));
-
-        noOfCandies = result[ratings.length -1];
-        for(int i = ratings.length - 2; i >= 0; i--) {
-            if(ratings[i] > ratings[i + 1]) {
-                result[i] = Math.max(result[i+1] + 1, result[i]);
-                noOfCandies += Math.max(result[i+1] + 1, result[i]);
-            } else {
-                noOfCandies += result[i];
-            }
-        }
-
-        System.out.println(Arrays.toString(result));
-
-        return noOfCandies;
+        return sum;
     }
 }
