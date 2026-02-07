892cb6a9adf6647d74c207a87aa11f71a90123aa
Mohamed Ameen <68771689+mohamedameen21@users.noreply.github.com>
2026-02-07 21:28:39 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (15.05%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -3,9 +3,11 @@ class Solution {
         int[] dp = new int[nums.length];
         Arrays.fill(dp, -1);
 
-        return getMaxSubsequence(nums.length - 1, nums, dp);
+        // return getMaxSubsequence(nums.length - 1, nums, dp);
+        return getMaxSubsequence(nums);
     }
 
+    // Memoization TC: O(N), SC: O(N) + O(N)
     private static int getMaxSubsequence(int i, int[] nums, int[] dp) {
         if (i == 0) {
             return nums[i];
@@ -25,5 +27,19 @@ class Solution {
         dp[i] = Math.max(pick, notPick);
 
         return dp[i];
+    }
+
+    private static int getMaxSubsequence(int[] nums) {
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            int pick = (i - 2 >= 0) ? dp[i - 2] + nums[i] : nums[i];
+            int notPick = dp[i - 1] + 0;
+
+            dp[i] = Math.max(pick, notPick);
+        }
+
+        return dp[nums.length - 1];
     }
 }
