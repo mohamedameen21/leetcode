1ed40d56a07c246896af2535d75e3cd71f5964f3
Mohamed Ameen <68771689+mohamedameen21@users.noreply.github.com>
2026-02-09 10:26:28 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (88.82%) - LeetSync

---
+++ 213-house-robber-ii/house-robber-ii.java
@@ -0,0 +1,69 @@
+class Solution {
+    public int rob(int[] nums) {
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, -1);
+
+        // return getMaxSubsequence(nums.length - 1, nums, dp);
+
+        if(nums.length == 1) return nums[0];
+
+        int[] includeFirst = Arrays.copyOfRange(nums, 0, nums.length-1);
+        int[] includeLast = Arrays.copyOfRange(nums, 1, nums.length);
+
+        return Math.max(getMaxSubsequence(includeFirst), getMaxSubsequence(includeLast));
+    }
+
+    // Memoization TC: O(N), SC: O(N) + O(N)
+    private static int getMaxSubsequence(int i, int[] nums, int[] dp) {
+        if (i == 0) {
+            return nums[i];
+        }
+
+        if (i < 0) {
+            return 0;
+        }
+
+        if (dp[i] != -1) {
+            return dp[i];
+        }
+
+        int pick = nums[i] + getMaxSubsequence(i - 2, nums, dp);
+        int notPick = 0 + getMaxSubsequence(i - 1, nums, dp);
+
+        dp[i] = Math.max(pick, notPick);
+
+        return dp[i];
+    }
+
+// Tabulation TC: O(N), SC: O(N)
+    // private static int getMaxSubsequence(int[] nums) {
+    //     int[] dp = new int[nums.length];
+    //     dp[0] = nums[0];
+
+    //     for (int i = 1; i < nums.length; i++) {
+    //         int pick = (i - 2 >= 0) ? dp[i - 2] + nums[i] : nums[i];
+    //         int notPick = dp[i - 1] + 0;
+
+    //         dp[i] = Math.max(pick, notPick);
+    //     }
+
+    //     return dp[nums.length - 1];
+    // }
+
+// Tabulation TC: O(N), SC: O(1)
+    private static int getMaxSubsequence(int[] nums) {
+        int prev2 = 0;
+        int prev1 = nums[0];
+
+        for(int i = 1; i < nums.length; i++) {
+            int pick = prev2 + nums[i];
+            int notPick = prev1 + 0;
+
+            int cur = Math.max(pick, notPick);
+            prev2 = prev1;
+            prev1 = cur;
+        }
+
+        return prev1;
+    }
+}
