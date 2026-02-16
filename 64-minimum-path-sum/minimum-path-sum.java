ea0698b9ffed677a1a0c18a3b8d4b6217d4854a8
Mohamed Ameen <68771689+mohamedameen21@users.noreply.github.com>
2026-02-16 08:45:03 +0530

    Time: 6 ms (10.60%) | Memory: 49.8 MB (95.17%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -8,27 +8,51 @@ class Solution {
             Arrays.fill(d, -1);
         }
 
-        return getMinPath(grid, m-1, n-1, dp);
+        // return getMinPath(grid, m-1, n-1, dp);
+
+        return getMinPath(grid);
     }
 
+    // Memoization TC: O(M * N) SC: O(N+M) + O(M*N)
     private static int getMinPath(int[][] grid, int i, int j, int[][] dp) {
-        if(i == 0 && j == 0) {
+        if (i == 0 && j == 0) {
             return grid[i][j];
         }
 
-        if(i < 0 || i >= grid.length || j < 0 || j >= grid[i].length) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[i].length) {
             return Integer.MAX_VALUE;
         }
 
-        if(dp[i][j] != -1) {
+        if (dp[i][j] != -1) {
             return dp[i][j];
         }
 
-        int up = getMinPath(grid, i -1, j, dp);
-        int left = getMinPath(grid, i, j-1, dp);
+        int up = getMinPath(grid, i - 1, j, dp);
+        int left = getMinPath(grid, i, j - 1, dp);
 
         dp[i][j] = Math.min(up, left) + grid[i][j];
 
         return dp[i][j];
+    }
+
+    // Tabulation
+    private static int getMinPath(int[][] grid) {
+        int[][] dp = new int[grid.length][grid[0].length];
+        dp[0][0] = grid[0][0];
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (i == 0 && j == 0) {
+                    continue;
+                }
+
+                int up = (i - 1 >= 0) ? dp[i - 1][j] : Integer.MAX_VALUE;
+                int left = (j - 1 >= 0) ? dp[i][j - 1] : Integer.MAX_VALUE;
+
+                dp[i][j] = Math.min(up, left) + grid[i][j];
+            }
+        }
+
+        return dp[grid.length - 1][grid[0].length - 1];
     }
 }
