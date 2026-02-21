7596b6beb1d9e0c48839e86948b7da6671b519e0
Mohamed Ameen <68771689+mohamedameen21@users.noreply.github.com>
2026-02-22 00:22:44 +0530

    Time: 4 ms (48.38%) | Memory: 45.8 MB (43.69%) - LeetSync

--- 120-triangle/triangle.java
+++ 120-triangle/triangle.java
@@ -5,7 +5,9 @@ class Solution {
             Arrays.fill(d, Integer.MIN_VALUE);
         }
 
-        return getMinPathSum(triangle, 0, 0, dp);
+        // return getMinPathSum(triangle, 0, 0, dp);
+
+        return getMinPathSum(triangle);
     }
 
     private static int getMinPathSum(List<List<Integer>> triangle, int i, int j, int[][] dp) {
@@ -23,5 +25,28 @@ class Solution {
         dp[i][j] = Math.min(down, downDigonal);
 
         return dp[i][j];
+    }
+
+    private static int getMinPathSum(List<List<Integer>> triangle) {
+        int[] dp = new int[triangle.get(triangle.size()-1).size()];
+
+        List<Integer> lastRow = triangle.get(triangle.size()-1);
+
+        for(int i = 0; i < lastRow.size(); i++) {
+            dp[i] = lastRow.get(i);
+        }
+
+        for(int i = triangle.size()-2; i >= 0; i--) {
+            for(int j = 0; j < triangle.get(i).size(); j++) {
+                int cell = triangle.get(i).get(j);
+
+                int down = dp[j];
+                int diagonal = dp[j+1];
+
+                dp[j] = cell + Math.min(down, diagonal);
+            }
+        }
+
+        return dp[0];
     }
 }
