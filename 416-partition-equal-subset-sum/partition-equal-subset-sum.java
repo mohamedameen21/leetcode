7063a26813d5a5e239d43756ff6e88824c8ba85f
Mohamed Ameen <68771689+mohamedameen21@users.noreply.github.com>
2026-03-06 02:11:04 +0530

    Time: 59 ms (39.81%) | Memory: 80.7 MB (9.71%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -7,17 +7,18 @@ class Solution {
         if(sum % 2 != 0) return false;
 
         int target = sum / 2;
-
-
         int[][] dp = new int[nums.length][target + 1];
 
         for(int[] d : dp) {
             Arrays.fill(d, -1);
         }
 
-        return isTargetExists(nums, nums.length - 1, target, dp);
+        // return isTargetExists(nums, nums.length - 1, target, dp);
+
+        return isTargetExists(nums, target);
     }
 
+// Memoization. TC: O(N*K) SC: O(N*K) + O(N)
     private static boolean isTargetExists(int[] arr, int index, int target, int[][] dp) {
         if(target == 0) {
             return true;
@@ -41,5 +42,30 @@ class Solution {
         dp[index][target] = (notTake || take) ? 1 : 0;
 
         return notTake || take;
+    }
+
+// Tabulation. TC:O(N*K) SC:(N*K)
+    private static boolean isTargetExists(int[] arr, int target) {
+        boolean[][] dp = new boolean[arr.length][target+1];
+
+        for(int i = 0; i < dp.length; i++) {
+            dp[i][0] = true; 
+        }
+        if(arr[0] <= target) dp[0][arr[0]] = true;
+
+        for(int i = 1; i < arr.length; i++) {
+            for(int j = 0; j <= target; j++) {
+                boolean notTake = dp[i-1][j];
+                boolean take = false;
+
+                if(j >= arr[i]) {
+                    take = dp[i-1][j-arr[i]];
+                }
+
+                dp[i][j] = notTake || take;
+            }
+        }
+
+        return dp[arr.length-1][target];
     }
 }
