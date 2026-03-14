2f2cfd3e4486bac600f484e4218dd390d5a8fd4d
Mohamed Ameen <68771689+mohamedameen21@users.noreply.github.com>
2026-03-15 03:14:25 +0530

    Time: 7 ms (65.27%) | Memory: 46.9 MB (25.61%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -15,27 +15,37 @@ class Solution {
         // (total - target) / 2 is odd -> then we can't able to find the subset (so the count is 0)
         // when it is even Then we need to find the subset with target (total - target) / 2
 
+        // int sum = 0;
+        // for (int n : nums) {
+        //     sum += n;
+        // }
+
+        // if ((sum - target) % 2 != 0)
+        //     return 0;
+
+        // target = (sum - target) / 2;
+
+        // if(target < 0) return 0;
+
+        // int[][] dp = new int[nums.length][target + 1];
+        // for (int[] d : dp) {
+        //     Arrays.fill(d, -1);
+        // }
+
+        // int totalSubsetThatGivesDifferenceEqualToTarget = count(nums, nums.length - 1, target, dp);
+
+
         int sum = 0;
         for (int n : nums) {
             sum += n;
         }
 
-        if ((sum - target) % 2 != 0)
-            return 0;
-
-        target = (sum - target) / 2;
-
-        if(target < 0) return 0;
-
-        int[][] dp = new int[nums.length][target + 1];
+        int[][] dp = new int[nums.length][2*sum + 1];
         for (int[] d : dp) {
             Arrays.fill(d, -1);
         }
 
-        // int s1Target = target;
-        // int s2Target = total - target;
-
-        int totalSubsetThatGivesDifferenceEqualToTarget = count(nums, nums.length - 1, target, dp);
+        int totalSubsetThatGivesDifferenceEqualToTarget = count(nums, nums.length - 1, target, dp, sum);
 
         return totalSubsetThatGivesDifferenceEqualToTarget;
     }
@@ -57,5 +67,24 @@ class Solution {
         dp[index][target] = take + notTake;
 
         return dp[index][target];
+    }
+
+    private static int count(int[] nums, int index, int target, int[][] dp, int offset) {
+        if (target > offset || target < -offset) return 0;
+
+        if(index < 0) {
+            return target == 0 ? 1 : 0;
+        }
+
+        if(dp[index][target + offset] != -1) {
+            return dp[index][target + offset];
+        }
+
+        int add = count(nums, index - 1, target + nums[index], dp, offset);
+        int subract = count(nums, index-1, target - nums[index], dp, offset);
+
+        dp[index][target + offset] = add + subract;
+
+        return dp[index][target + offset];
     }
 }
