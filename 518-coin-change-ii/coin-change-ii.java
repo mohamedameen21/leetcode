684b551e2de575de0f1e081740c78647d28577bf
Mohamed Ameen <68771689+mohamedameen21@users.noreply.github.com>
2026-03-16 01:46:45 +0530

    Time: 11 ms (61.91%) | Memory: 56.9 MB (40.69%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -1,36 +1,38 @@
 class Solution {
     public int change(int amount, int[] coins) {
         int[][] dp = new int[coins.length][amount + 1];
-        for(int[] d : dp) {
+        for (int[] d : dp) {
             Arrays.fill(d, -1);
         }
 
         // return count(coins, coins.length-1, amount, dp);
-
-        return count(coins, amount);
+        // return count(coins, amount);
+        return countO(coins, amount);
     }
 
+    // Memoization
     private static int count(int[] coins, int index, int target, int[][] dp) {
-        if(target == 0) {
+        if (target == 0) {
             return 1;
         }
 
-        if(index < 0 || target < 0) {
+        if (index < 0 || target < 0) {
             return 0;
         }
 
-        if(dp[index][target] != -1) {
+        if (dp[index][target] != -1) {
             return dp[index][target];
         }
 
         int take = count(coins, index, target - coins[index], dp);
-        int notTake = count(coins, index -1, target, dp);
+        int notTake = count(coins, index - 1, target, dp);
 
         dp[index][target] = take + notTake;
 
         return dp[index][target];
     }
 
+    // Tabulation
     private static int count(int[] coins, int target) {
         int n = coins.length;
         int[][] dp = new int[n][target + 1];
@@ -40,7 +42,6 @@ class Solution {
             dp[i][0] = 1;
         }
 
-
         // first row: using only coins[0]
         for (int t = 1; t <= target; t++) {
             if (t % coins[0] == 0) {
@@ -48,15 +49,38 @@ class Solution {
             }
         }
 
-        for(int index = 1; index < coins.length; index++) {
-            for(int t = 0; t <= target; t++) {
+        for (int index = 1; index < coins.length; index++) {
+            for (int t = 0; t <= target; t++) {
                 int take = (coins[index] <= t) ? dp[index][t - coins[index]] : 0;
-                int notTake = dp[index -1][t];
+                int notTake = dp[index - 1][t];
 
                 dp[index][t] = take + notTake;
             }
         }
 
-        return dp[coins.length-1][target];
+        return dp[coins.length - 1][target];
+    }
+
+    // Space Optimiation
+    private static int countO(int[] coins, int target) {
+        int[] dp = new int[target + 1];
+        dp[0] = 1;
+
+        for (int t = 1; t <= target; t++) {
+            if (t % coins[0] == 0) {
+                dp[t] = 1;
+            }
+        }
+
+        for (int index = 1; index < coins.length; index++) {
+            for (int t = coins[index]; t <= target; t++) {
+                int take = dp[t - coins[index]];
+                int notTake = dp[t];
+
+                dp[t] = take + notTake;
+            }
+        }
+
+        return dp[target];
     }
 }
