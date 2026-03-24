1ffb5246801c739f28507539b3f46d887a175c24
Mohamed Ameen <68771689+mohamedameen21@users.noreply.github.com>
2026-03-24 11:38:01 +0530

    Time: 24 ms (33.17%) | Memory: 62.7 MB (5.06%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,63 +1,81 @@
 class Solution {
-public int longestCommonSubsequence(String text1, String text2) {
-    int[][] dp = new int[text1.length()][text2.length()];
-    for (int[] d : dp) {
-        Arrays.fill(d, -1);
+    public int longestCommonSubsequence(String text1, String text2) {
+        int[][] dp = new int[text1.length()][text2.length()];
+        for (int[] d : dp) {
+            Arrays.fill(d, -1);
+        }
+
+        // return getMax(text1, text1.length() - 1, text2, text2.length() - 1, dp);
+        return getMax(text1, text2);
     }
 
-    // return getMax(text1, text1.length() - 1, text2, text2.length() - 1, dp);
-    return getMax(text1, text2);
-}
+    private static int getMax(String s1, int index1, String s2, int index2, int[][] dp) {
+        if (index1 < 0 || index2 < 0) {
+            return 0;
+        }
 
-private static int getMax(String s1, int index1, String s2, int index2, int[][] dp) {
-    if (index1 < 0 || index2 < 0) {
-        return 0;
-    }
+        if (dp[index1][index2] != -1) {
+            return dp[index1][index2];
+        }
 
-    if (dp[index1][index2] != -1) {
+        if (s1.charAt(index1) == s2.charAt(index2)) {
+            dp[index1][index2] = 1 + getMax(s1, index1 - 1, s2, index2 - 1, dp);
+
+            return dp[index1][index2];
+        }
+
+        dp[index1][index2] = Math.max(getMax(s1, index1 - 1, s2, index2, dp), getMax(s1, index1, s2, index2 - 1, dp));
+
         return dp[index1][index2];
     }
 
-    if (s1.charAt(index1) == s2.charAt(index2)) {
-        dp[index1][index2] = 1 + getMax(s1, index1 - 1, s2, index2 - 1, dp);
+    // private static int getMax(String s1, String s2) {
+    //     int[][] dp = new int[s1.length()][s2.length()];
 
-        return dp[index1][index2];
-    }
+    //     for (int i = 0; i < s2.length(); i++) {
+    //         if (s1.charAt(0) == s2.charAt(i)) {
+    //             dp[0][i] = 1;
+    //         } else {
+    //             dp[0][i] = (i > 0) ? dp[0][i - 1] : 0;
+    //         }
+    //     }
 
-    dp[index1][index2] = Math.max(getMax(s1, index1 - 1, s2, index2, dp), getMax(s1, index1, s2, index2 - 1, dp));
+    //     for (int i = 0; i < s1.length(); i++) {
+    //         if (s2.charAt(0) == s1.charAt(i)) {
+    //             dp[i][0] = 1;
+    //         } else {
+    //             dp[i][0] = (i > 0) ? dp[i - 1][0] : 0;
+    //         }
+    //     }
 
-    return dp[index1][index2];
-}
+    //     for (int i = 1; i < s1.length(); i++) {
+    //         for (int j = 1; j < s2.length(); j++) {
+    //             if (s1.charAt(i) == s2.charAt(j)) {
+    //                 dp[i][j] = 1 + dp[i - 1][j - 1];
+    //             } else {
+    //                 dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
+    //             }
+    //         }
+    //     }
 
-private static int getMax(String s1, String s2) {
-    int[][] dp = new int[s1.length()][s2.length()];
+    //     return dp[s1.length() - 1][s2.length() - 1];
+    // }
 
-    for(int i = 0; i < s2.length(); i++) {
-        if(s1.charAt(0) == s2.charAt(i)) {
-            dp[0][i] = 1;
-        } else {
-            dp[0][i] = (i > 0) ? dp[0][i-1] : 0;
-        }
-    }
+    private static int getMax(String s1, String s2) {
+        int[][] dp = new int[s1.length() + 1][s2.length() + 1];
 
-    for(int i = 0; i < s1.length(); i++) {
-        if(s2.charAt(0) == s1.charAt(i)) {
-            dp[i][0] = 1;
-        } else {
-            dp[i][0] = (i > 0) ? dp[i-1][0] : 0;
-        }
-    }
+        for (int i = 1; i <= s1.length(); i++) {
+            for (int j = 1; j <= s2.length(); j++) {
 
-    for(int i = 1; i < s1.length(); i++) {
-        for(int j = 1; j < s2.length(); j++) {
-            if(s1.charAt(i) == s2.charAt(j)) {
-                dp[i][j] = 1 + dp[i-1][j-1];
-            } else {
-                dp[i][j] = Math.max(dp[i][j-1], dp[i-1][j]);
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+
             }
         }
-    }
 
-    return dp[s1.length() -1][s2.length() - 1];  
-}
+        return dp[s1.length()][s2.length()];
+    }
 }
