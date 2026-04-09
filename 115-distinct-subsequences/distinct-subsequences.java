01f7da1c68a23754ade6391530c2371ba54a525e
Mohamed Ameen <68771689+mohamedameen21@users.noreply.github.com>
2026-04-09 12:21:10 +0530

    Time: 18 ms (77.91%) | Memory: 54.3 MB (56.95%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -26,23 +26,41 @@ class Solution {
         }
     }
 
-    private static int countSubsequence(String s1, String s2) {
-        int[][] dp = new int[s1.length()+1][s2.length()+1];
+    // private static int countSubsequence(String s1, String s2) {
+    //     int[][] dp = new int[s1.length()+1][s2.length()+1];
 
-        for(int i = 0; i <= s1.length(); i++) {
-            dp[i][0] = 1;
-        }
+    //     for(int i = 0; i <= s1.length(); i++) {
+    //         dp[i][0] = 1;
+    //     }
+
+    //     for(int i = 1; i <= s1.length(); i++) {
+    //         for(int j = 1; j <= s2.length(); j++) {
+    //             if(s1.charAt(i-1) == s2.charAt(j-1)) {
+    //                 dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+    //             } else {
+    //                 dp[i][j] = dp[i-1][j];
+    //             }
+    //         }
+    //     }
+
+    //     return dp[s1.length()][s2.length()];
+    // }
+
+    private static int countSubsequence(String s1, String s2) {
+        int[] dp = new int[s2.length()+1];
+        dp[0] = 1;
+
 
         for(int i = 1; i <= s1.length(); i++) {
-            for(int j = 1; j <= s2.length(); j++) {
+            for(int j = s2.length(); j >= 1; j--) {
                 if(s1.charAt(i-1) == s2.charAt(j-1)) {
-                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+                    dp[j] = dp[j-1] + dp[j];
                 } else {
-                    dp[i][j] = dp[i-1][j];
+                    dp[j] = dp[j];
                 }
             }
         }
 
-        return dp[s1.length()][s2.length()];
+        return dp[s2.length()];
     }
 }
