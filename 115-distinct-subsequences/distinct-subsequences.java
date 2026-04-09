7ca5afd938af673a5a7bcbbea21f04c64a3df17a
Mohamed Ameen <68771689+mohamedameen21@users.noreply.github.com>
2026-04-09 12:09:50 +0530

    Time: 23 ms (16.77%) | Memory: 63.4 MB (9.39%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -6,7 +6,9 @@ class Solution {
             Arrays.fill(d, -1);
         }
 
-        return countSubsequence(s, s.length()-1, t, t.length()-1, dp);
+        // return countSubsequence(s, s.length()-1, t, t.length()-1, dp);
+
+        return countSubsequence(s, t);
     }
 
     private static int countSubsequence(String s1, int i, String s2, int j, int[][] dp) {
@@ -22,5 +24,25 @@ class Solution {
             dp[i][j] = countSubsequence(s1, i-1, s2, j, dp);
             return dp[i][j];
         }
+    }
+
+    private static int countSubsequence(String s1, String s2) {
+        int[][] dp = new int[s1.length()+1][s2.length()+1];
+
+        for(int i = 0; i <= s1.length(); i++) {
+            dp[i][0] = 1;
+        }
+
+        for(int i = 1; i <= s1.length(); i++) {
+            for(int j = 1; j <= s2.length(); j++) {
+                if(s1.charAt(i-1) == s2.charAt(j-1)) {
+                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+                } else {
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+
+        return dp[s1.length()][s2.length()];
     }
 }
